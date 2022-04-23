831dfde1cb81b66aac27741159bc23981c8ea6e5
Wiryono Lauw <wiryonolau@gmail.com>
2022-04-23 20:43:45 +0700

    Add ipchandler, react devtools

--- {{cookiecutter.project_slug}}/src/main.js
+++ {{cookiecutter.project_slug}}/src/main.js
@@ -1,14 +1,29 @@
+const { ContainerBuilder, YamlFileLoader } = require('node-dependency-injection');
 const { app, ipcMain, ipcRenderer, BrowserWindow } = require('electron');
 const path = require('path');
-
 const isDev = require('electron-is-dev');
 
-try {
-    if (isDev) {
-        require('electron-reloader')(module);
-    }
-} catch {
+if (isDev) {
+    require('electron-reloader')(module);
+
+    app.setPath("appData", path.join(path.normalize(__dirname + "/../"), ".config"));
+
+    const { default: installExtension, REACT_DEVELOPER_TOOLS }  = require('electron-devtools-installer');
+    app.whenReady().then(() => {
+        installExtension(REACT_DEVELOPER_TOOLS, {
+            allowFileAccess: true
+        });
+    }).then((name) => {
+        console.log(`Added Extension:  ${name}`);
+    }).catch((err) => {
+        console.log('An error occurred: ', err);
+    });
 }
+
+const container = new ContainerBuilder();
+const config_dir = path.join(path.normalize(__dirname + "/../"), "config");
+const loader = new YamlFileLoader(container);
+loader.load(path.join(config_dir, "service.yml"));
 
 const createWindow = function() {
   let mainWindow = new BrowserWindow({
@@ -48,3 +63,15 @@ app.on("activate", () => {
     }
 });
 
+ipcMain.on('service', (event, request) => {
+  try {
+    let cls = this.container.get(request.service);
+    if(typeof cls[request.function] === "function") {
+      // ...request.args same as *request.args in python
+      let result = cls[request.function](...request.args);
+      event.sender.send(request.channel, result);
+    }
+  } catch(e) {
+    console.log(e);
+  }
+});
